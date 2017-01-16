28e999eaffd526a490a343a13dbbdc18f735f063
Evan Weinberg <evansweinberg@gmail.com>
2017-01-16 18:20:51 -0500

    Updated various single node build scripts.

--- single/qdp.sh
+++ single/qdp.sh
@@ -3,7 +3,7 @@
 LOCALDIR=$1;
 
 cd build/qdp
-../../src/qdp/configure --prefix=$(LOCALDIR)/install/qdp --enable-sse2 CC=gcc  --with-qmp=$(LOCALDIR)/install/qmp --with-qio=$(LOCALDIR)/install/qio --with-qla=$(LOCALDIR)/install/qla CFLAGS="-Wall -O3 -std=gnu99 -march=native -msse2 -mfpmath=sse -ffast-math -funroll-loops -fprefetch-loop-arrays -fomit-frame-pointer -fopenmp"
+../../src/qdp/configure --prefix=${LOCALDIR}/install/qdp --enable-sse2 CC=gcc  --with-qmp=${LOCALDIR}/install/qmp --with-qio=${LOCALDIR}/install/qio --with-qla=${LOCALDIR}/install/qla CFLAGS="-Wall -O3 -std=gnu99 -march=native -msse2 -mfpmath=sse -ffast-math -funroll-loops -fprefetch-loop-arrays -fomit-frame-pointer -fopenmp"
 make -j8
 make install
 make check
--- single/qopqdp.sh
+++ single/qopqdp.sh
@@ -3,7 +3,7 @@
 LOCALDIR=$1;
 
 cd build/qopqdp
-../../src/qopqdp/configure --prefix=$(LOCALDIR)/install/qopqdp --enable-sse2 CC=gcc  --with-qmp=$(LOCALDIR)/install/qmp --with-qio=$(LOCALDIR)/install/qio --with-qla=$(LOCALDIR)/install/qla --with-qdp=$(LOCALDIR)/install/qdp CFLAGS="-Wall -O3 -std=gnu99 -march=native -msse2 -mfpmath=sse -ffast-math -funroll-loops -fprefetch-loop-arrays -fomit-frame-pointer -fopenmp"
+../../src/qopqdp/configure --prefix=${LOCALDIR}/install/qopqdp --enable-sse2 CC=gcc  --with-qmp=${LOCALDIR}/install/qmp --with-qio=${LOCALDIR}/install/qio --with-qla=${LOCALDIR}/install/qla --with-qdp=${LOCALDIR}/install/qdp CFLAGS="-Wall -O3 -std=gnu99 -march=native -msse2 -mfpmath=sse -ffast-math -funroll-loops -fprefetch-loop-arrays -fomit-frame-pointer -fopenmp"
 make -j8
 make install
 make check
